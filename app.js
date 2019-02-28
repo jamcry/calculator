5ef999f7b3c9f6010fbf957acb551ac73525ffd9
jamcry <jamcry@hotmail.com>
2019-02-28 13:37:39 +0300

    Seperate CSS, update layout and script. Added basic logic.

--- app.js
+++ app.js
@@ -1,4 +1,66 @@
+const display = document.querySelector(".display");
+const numBtns = document.querySelectorAll(".num-btn");
+const decBtn = document.querySelector(".dec-btn");
+const opBtns = document.querySelectorAll(".op-btn");
+const eqBtn = document.querySelector(".eq-btn");
 
+let inputArr = [];
+
+numBtns.forEach(btn => btn.addEventListener("click", () => {
+    updateDisplay(btn.innerHTML);
+    console.warn("a (num-btn) is clicked.")
+    console.log(`Displaying its value (${btn.innerHTML})`);
+}));
+
+decBtn.addEventListener("click", () => {
+    if(!display.value.includes(".")) {
+        updateDisplay(".");
+    }
+    else {
+        alert("Too many decimals!");
+    }
+});
+opBtns.forEach(btn => btn.addEventListener("click", () => {
+    if(btn.innerHTML === "AC") {
+        clearAll();
+    }
+    else if(btn.innerHTML === "CL") {
+        console.log("CL is clicked");
+        clear();
+    }
+    else {
+            inputArr.push(parseFloat(display.value));
+            inputArr.push(btn.innerHTML);
+            lg(`display val: ${display.value}`);
+            lg(`input array: ${JSON.stringify(inputArr)}`);
+            clearDisplay();
+
+    }
+}));
+
+eqBtn.addEventListener("click", () => {
+    inputArr.push(parseFloat(display.value));
+    clearDisplay();
+    operate();
+});
+function operate() {
+    lg(JSON.stringify(inputArr));
+    // Use two for loops to operate
+    // First for * and /, second for + and -.
+}
+function clearAll(){
+    display.value = "";
+    inputArr = [];
+}
+function clear() {
+    display.value = display.value.slice(0, -1);
+}
+function clearDisplay(){
+    display.value = "";
+}
+function updateDisplay(value) {
+    display.value += value;
+}
 
 function add(a,b) {
     return a+b;
@@ -12,6 +74,4 @@ function multiply(a,b) {
 function divide(a,b) {
     return a/b;
 }
-function operate(op, a, b) {
-    op(a,b);
-}
+const lg = (msg) => console.log(msg);
